d251441ebcc72f505c02be90554cc9e31fd47d68
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-11-12 16:18:55 +0700

    update MukeshRobot/modules/gen.py

--- MukeshRobot/modules/gen.py
+++ MukeshRobot/modules/gen.py
@@ -31,7 +31,7 @@ from telethon.sessions import StringSession
 from telethon.tl.functions.channels import JoinChannelRequest
 from pyromod.listen.listen import ListenerTimeout
 
-from config import OWNER_ID
+from MukeshRobot.config import OWNER_ID
 from MukeshRobot import Client
 from MukeshRobot.modules.alive import retry_key
 
