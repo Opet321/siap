b06c3740c2a2e66bb21ccc69525c889eaed48cd8
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-11-12 14:19:41 +0700

    update MukeshRobot/__main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -214,7 +214,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name            
             update.effective_message.reply_photo(            
                 photo=START_IMG,
-                caption=PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME,sql.num_users(),sql.num_chats()),
+                caption=PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(uptime), BOT_NAME,sql.num_users(), sql.num_chats()),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
